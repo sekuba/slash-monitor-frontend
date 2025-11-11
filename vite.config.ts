00a4c723b4fe3b8f525d210c6dc0f68a51ec3192
sekuba <29250140+sekuba@users.noreply.github.com>
2025-11-11 20:52:20 +0100

    reename

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/slash-monitor-frontend/',
+  base: '/slashmon/',
   server: {
     port: 5173,
   },
